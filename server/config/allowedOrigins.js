cbefee9537f6fca31674fd6c0cad86f6afc5add3
zeeshanalico <zeeshanalico24@gmail.com>
2025-04-05 02:02:48 +0500

    vercel config and allowed all origings

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-  "https://tashfeen-blogify.vercel.app",
-  "http://localhost:5173",
-  "http://localhost:3001"
+  "*"
+  // "http://localhost:5173",
+  // "http://localhost:3001"
 ]
 
 module.exports = allowedOrigins;
